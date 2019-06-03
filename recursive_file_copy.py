f25c6b96f58357da32ec6692c35f24480e30a194
rwDocs-CO <sean.london@roguewave.com>
2019-06-03 11:18:43 -0600

    Updating the licensing
    Just some updates to the licensing so that it is clear.

--- recursive_file_copy.py
+++ recursive_file_copy.py
@@ -20,7 +20,7 @@
 ######################################
 # MIT License
 #
-# Copyright(c) Rogue Wave Software, Inc. All rights reserved.
+# Copyright(c) Rogue Wave Software, Inc. 
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
